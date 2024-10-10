44f396abc9dfb2087e97e2d9d851c15cf3b112fe
Delfics <Soulspirit95@gmail.com>
2024-10-10 12:52:34 +0300

    Тесты пройдены

--- src/main/java/ru/practicum/shareit/user/Jpa/UserStorageJpa.java
+++ src/main/java/ru/practicum/shareit/user/Jpa/UserStorageJpa.java
@@ -1,7 +1,13 @@
 package ru.practicum.shareit.user.Jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.user.User;
 
 public interface UserStorageJpa extends JpaRepository<User, Long> {
+
+    @Query("SELECT COUNT(user) > 0 " +
+            "FROM User as user " +
+            "WHERE user.email = ?1")
+    Boolean existsByEmail(String email);
 }
